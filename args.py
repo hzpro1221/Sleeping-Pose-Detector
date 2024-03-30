d44f72afe9647bccc6d7c71db7c14b9ab3544281
Khai Duc <hzpro1221>
2024-03-30 16:45:58 +0700

    add

---
+++ args.py
@@ -0,0 +1,8 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--PATH',
+        type=str,
+    )
--- inference.py
+++ inference.py
@@ -2,8 +2,9 @@ import torch
 from skimage import io
 
 from model import CNN
+from args import parse_arguments
 
-def inference(PATH=""):
+def inference(args):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,8 +14,22 @@ def inference(PATH=""):
     model.to(device)
 
     with torch.no_grad():
-        image = io.imread(PATH)
+        image = io.imread(args.PATH)
         logits = model.forward(image)
         pred = torch.argmax(logits).item()
     
     return pred
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    pred = inference(args)
+    if (pred==0):
+        print("Tư thế ngủ không xấu")
+    if (pred==1):
+        print("Tư thế ngủ bào thai")
+    if (pred==2):
+        print("Nằm ngửa hai tay giơ lên đầu")
+    if (pred==3):
+        print("Ngủ sấp")
+    if (pred==4):
+        print("Ngủ tựa đầu lên cánh tay")
